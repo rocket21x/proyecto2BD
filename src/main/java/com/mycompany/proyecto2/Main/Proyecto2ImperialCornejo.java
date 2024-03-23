4d8b3d8f45b5d625b0db5f37f00c91c1d1164cb3
Jesus imperial <jeskid99@gmail.com>
2024-03-22 22:09:14 -0700

    Creacion del repositorio

---
+++ src/main/java/com/mycompany/proyecto2/Main/Proyecto2ImperialCornejo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto2.Main;
+
+/**
+ *
+ * @author jesus
+ */
+public class Proyecto2ImperialCornejo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
