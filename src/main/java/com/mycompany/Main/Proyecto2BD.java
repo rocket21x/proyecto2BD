36c191111d2575ad0f2395bfcaf96e3a502518c6
Jesus imperial <jeskid99@gmail.com>
2024-03-23 10:37:34 -0700

    Creacion de las interfaces graficas menu y registrar licencia

--- src/main/java/com/mycompany/Main/Proyecto2BD.java
+++ src/main/java/com/mycompany/Main/Proyecto2BD.java
@@ -2,15 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.proyecto2.Main;
+package com.mycompany.Main;
+
+import Formularios.MenuGUI;
 
 /**
  *
  * @author jesus
  */
-public class Proyecto2ImperialCornejo {
+public class Proyecto2BD {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        MenuGUI menu = new MenuGUI();
+        menu.setVisible(true);
     }
 }
